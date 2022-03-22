37a0f8e457b7e9997cc4d3bd9a47729e7b74a880
hayratjan <1693305172@qq.com>
2022-03-22 14:42:09 +0800

    hayratjan

--- login/admin.py
+++ login/admin.py
@@ -12,26 +12,32 @@ admin.site.index_title = "喀什大学图书馆预约管理系统"
 
 class StudentsManager(admin.ModelAdmin):
     # 列表页显示那些字段
+    search_fields = ('name','phone')
     list_display = ['id', 'name', 'password', 'phone', 'email', 'time', 'is_active', 'admin_sample']
 
 
 class BookingsManager(admin.ModelAdmin):
     # 列表页显示那些字段
+    search_fields = ('students', 'number')
     list_display = ['id', 'students', 'number', 'room', 'period', 'time', 'is_active', 'admin_sample']
 
 
 class RoomsManager(admin.ModelAdmin):
     # 列表页显示那些字段
+    search_fields = ('name',)
+    search_fields=('name',)
     list_display = ['id', 'name', 'number', 'time', 'is_active', 'admin_sample']
 
 
 class IntegralsManager(admin.ModelAdmin):
     # 列表页显示那些字段
+    search_fields = ('student',)
     list_display = ['id', 'student', 'title', 'text', 'time', 'is_active', 'admin_sample']
 
 
 class TextsManager(admin.ModelAdmin):
     # 列表页显示那些字段
+    search_fields = ('title',)
     list_display = ['id', 'title', 'time', 'is_active']
 
 
